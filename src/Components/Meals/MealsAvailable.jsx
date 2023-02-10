5334d940856537a25c4a4e849af4ed3889c1a723
priyammedhi <priyamkar23@gmail.com>
2023-02-10 14:51:52 +0530

    deleted

--- src/Components/Meals/MealsAvailable.jsx
+++ src/Components/Meals/MealsAvailable.jsx
@@ -5,27 +5,27 @@ import MealItem from "./MealItems/MealItem";
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Dum Gosht Biryani",
-    description: "The Pakistani celebrated dish of basmati rice and mutton united with fragrant spices and garden fresh herbs",
-    price: 850,
+    name: "Aloo Paratha",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas magni officia neque itaque. Nemo culpa quod iure facilis molestias similique",
+    price: 30,
   },
   {
     id: "m2",
-    name: "Murgh Mughlai Biryani",
-    description: "Influenced by the imperial kitchens of the Mughul empire, basmati rice cooked together with chicken in an affluent gravy of aromatic spices and fresh herbs",
-    price: 850,
+    name: "Chole Bature",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas magni officia neque itaque. Nemo culpa quod iure facilis molestias similique",
+    price: 60,
   },
   {
     id: "m3",
-    name: "Bhuna Gosht",
-    description: "Boneless mutton cubes cooked with onions, tomatoes, local spices and finished with black pepper",
-    price: 1095,
+    name: "Chole Samosa",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas magni officia neque itaque. Nemo culpa quod iure facilis molestias similique",
+    price: 20,
   },
   {
     id: "m4",
-    name: "Chicken Karahi",
-    description: "An iconic Pakistani dish of chicken cooked on bone in traditional style tomato gravy seasoned with green chilies and black pepper",
-    price: 850,
+    name: "Aloo Tikki Rice Bowl",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas magni officia neque itaque. Nemo culpa quod iure facilis molestias similique",
+    price: 80,
   },
 ];
 const MealsAvailable = () => {
