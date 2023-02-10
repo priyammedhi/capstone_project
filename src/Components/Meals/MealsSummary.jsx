feebd47205f84a87662eab5d7fe1fdd1525cf330
priyammedhi <priyamkar23@gmail.com>
2023-02-10 14:24:45 +0530

    hi

---
+++ src/Components/Meals/MealsSummary.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from'./MealsSummary.module.css'
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+        <h2>You Order We Deliver</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae atque eveniet assumenda earum fugit rerum illo dignissimos. Cumque, iste expedita.
+        </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ut doloremque earum repellat error expedita magni excepturi laboriosam aperiam culpa.
+        </p>
+        <h2>Taste the Best</h2>
+      </section>
+    )
+}
+
+export default MealsSummary
